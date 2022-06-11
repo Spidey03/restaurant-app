0b3bdbf13992b18f767ed22c7802b890a165e66a
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-11 16:16:49 +0530

    test: check user exist storage

--- django_restaurant/restaurant/storages/user_storage_implementation.py
+++ django_restaurant/restaurant/storages/user_storage_implementation.py
@@ -5,7 +5,9 @@ from restaurant.interactors.storages.user_storages_interface import UserStorageI
 class UserStorageImplementation(UserStorageInterface):
 
     def check_user_exists(self, user_id: str):
-        pass
+        from restaurant.models import User
+
+        return User.objects.filter(id=user_id).exists()
 
     def is_mobile_number_already_registered(self, mobile_number: str):
         from restaurant.models import User
@@ -42,4 +44,23 @@ class UserStorageImplementation(UserStorageInterface):
         return user_obj
 
     def get_user(self, user_id) -> UserDetailsDTO:
-        pass
+        from restaurant.models import User
+        from restaurant.exceptions.exceptions import UserNotFoundException
+
+        if not User.objects.filter(id=user_id).exists():
+            raise UserNotFoundException()
+        user_details = User.objects.get(id=user_id)
+        user_dto = self._get_user_details_dto(user_details)
+        return user_dto
+
+    @staticmethod
+    def _get_user_details_dto(user_details) -> UserDetailsDTO:
+        user_dto = UserDetailsDTO(
+            id=str(user_details.id),
+            username=str(user_details.username),
+            first_name=user_details.first_name,
+            last_name=user_details.last_name,
+            mobile_number=user_details.mobile_number,
+            date_joined=str(user_details.date_joined.replace(tzinfo=None)),
+        )
+        return user_dto
