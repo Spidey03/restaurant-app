0e372bdd74fca8128344ccbf06d5f243ecad4acf
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-11 13:18:23 +0530

    Add table models

---
+++ django_restaurant/restaurant/models/table.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+from restaurant.models import User, Cart, Order, Restaurant
+
+
+class Table(models.Model):
+    id = models.UUIDField(primary_key=True)
+    restaurant = models.ForeignKey(
+        Restaurant,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return {self.id}
+
+
+class TableCart(models.Model):
+    id = models.UUIDField(primary_key=True)
+    table_id = models.ForeignKey(
+        Table, null=False, blank=False,
+        on_delete=models.CASCADE
+    )
+    user_id = models.ForeignKey(
+        User, null=True, blank=False,
+        on_delete=models.CASCADE
+    )
+    cart_id = models.ForeignKey(
+        Cart, null=True, blank=False,
+        on_delete=models.CASCADE
+    )
+
+
+class TableOrder(models.Model):
+    id = models.UUIDField(primary_key=True)
+    table_id = models.ForeignKey(
+        Table, null=False, blank=False,
+        on_delete=models.CASCADE
+    )
+    user_id = models.ForeignKey(
+        User, null=True, blank=False,
+        on_delete=models.CASCADE
+    )
+    order_id = models.ForeignKey(
+        Order, null=True, blank=False,
+        on_delete=models.CASCADE
+    )
