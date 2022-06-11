9b9e357240f9af778c2803f687280a2956a6e748
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-06-11 15:59:25 +0530

    models

--- django_restaurant/restaurant/models/__init__.py
+++ django_restaurant/restaurant/models/__init__.py
@@ -1,14 +1,5 @@
-from user import User
+from restaurant.models.user import User
+from restaurant.models.item import Item
 from restaurant.models.order import Order, Cart
 from restaurant.models.restaurant import Restaurant
 from restaurant.models.table import Table, TableCart, TableOrder
-
-__all__ = [
-    'User',
-    'Restaurant',
-    'Order',
-    'Cart',
-    'Table',
-    'TableCart',
-    'TableOrder',
-]
